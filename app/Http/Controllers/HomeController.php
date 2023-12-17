03436982e06bb592cc3def9fb3dcff2b374ee0cb
Marco Julian <marcos_019@hotmail.com>
2023-12-16 20:37:07 -0600

    Section 13: One to one Relationship

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Post;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -12,8 +13,9 @@ class HomeController extends Controller
      * Handle the incoming request.
      */
     public function __invoke(Request $request)
-    {    
+    {
+        $users = User::all();
 
-
+        return view('home', compact('users'));
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,32 +2,17 @@
 
 @section('content')
 <main role="main" class="container">
-    <h1 class="mt-5 text-danger">Home</h1>
-    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cum pariatur ratione quaerat vero a, ullam reiciendis earum distinctio nihil exercitationem quidem neque odit aliquid quasi esse, repudiandae, adipisci non placeat.
-  
     <div class="row mt-5">
-        @foreach ($blogs as $blog)
-        @if ($blog['status'] == 1)
-            <div class="col-md-4">
-                <div class="card">
-                    <div class="card-body">
-                        <h2>{{$blog['title']}}</h2>
-                        <p>{{$blog['body']}}</p>
-
-                    </div>
+        @foreach ($users as $user)
+        <div class="col-md-3">
+            <div class="card">
+                <div class="card-body">
+                    <h4>{{$user->name}}</h4>
+                    <p>{{$user->email}}</p>
+                    <p>{{$user->address->addresse}}</p>
                 </div>
             </div>
-            @else
-            <div class="col-md-4">
-                <div class="card">
-                    <div class="card-body">
-                        <h2>{{$blog['title']}}</h2>
-                        <p>{{$blog['body']}}</p>
-                        <div class="btn-sm btn-warning">Pending</div>
-                    </div>
-                </div>
-            </div>         
-        @endif        
+        </div>            
         @endforeach
     </div>
 </main>
